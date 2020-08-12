230f70343483a11f6dfc4335cb24b8f238f4199c
Satyam Agarwal <sagarwal@ask-fast.com>
2020-08-12 11:55:43 +0530

    Different Environment file

--- src/main/java/com/redis/config/RedisConfig.java
+++ src/main/java/com/redis/config/RedisConfig.java
@@ -1,23 +1,38 @@
 package com.redis.config;
 
 import com.redis.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
-    @Bean
+
+    @Value("${redis.host}")
+    private String host;
+    @Value("${redis.port}")
+    private int port;
+  /*  @Bean
     LettuceConnectionFactory lettuceConnectionFactory(){
-        return new LettuceConnectionFactory();
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        lettuceConnectionFactory.setHostName("127.0.0.1");
+        lettuceConnectionFactory.setPort(6379);
+        return lettuceConnectionFactory;
+    }*/
+
+    @Bean
+    LettuceConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host,port));
     }
     @Bean
     RedisTemplate redisTemplate(){
         RedisTemplate redisTemplate = new RedisTemplate();
-        redisTemplate.setConnectionFactory(lettuceConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
 }
